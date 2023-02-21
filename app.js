32fd2664c4ccbd41670d3aba8513b8cf5c824970
moha <moha729g@stud.kea.dk>
2023-02-21 16:50:17 +0100

    POST-PATCH-DELETE

--- app.js
+++ app.js
@@ -1,41 +1,81 @@
-const express = require("express")
-const app = express()
+const express = require("express")//import express
+const app = express()//instanciate express
 
-console.log('program is running...')
+app.use(express.json())//Access the json library within expree
 
-function getBirds() {
-    const birds = []
+const birds = []//instanciate an in-memory empty array
 
-    const bird1 = {
-        type: "blackbird"
+let nextId = birds.length//simualating the auto_increment fucntion from MySql id_column
+
+function getNextId() {
+    return ++nextId 
+}//automatically incrementing the index
+
+getDefaultBirds()
+
+function getDefaultBirds() {
+    if(birds.length === 0){
+        makeBird(getNextId(), "blackbird", 'Turdus', 'merula')
+        makeBird(getNextId(), "Chaffinch", 'Fringilla', 'coelebs')
+        makeBird(getNextId(), "Greenish", 'Phylloscopus', 'trochiloides')
+        makeBird(getNextId(), "House sparrow", 'Passer', 'domesticus')     
     }
-    const bird2 = {
-        type: "Chaffinch"
-    }
-    const bird3 = {
-        type: "Greenish"
-    }
-
-    birds[0] = bird1
-    birds[1] = bird2
-    birds[2] = bird3
-
-    return birds
 }
+
+function getBird(birds, id) {
+    return birds.find(bird => bird.id === Number(id))
+}
+
+function makeBird(id, type, family, breed){
+    const bird = {
+        id: id,
+        type: type,
+        family: family,
+        breed: breed
+    }
+    birds.push(bird)
+    return bird
+}
+
+//REDIRECT  
+app.get("/", (req, res) => {
+    res.redirect('/birds')
+})
 
 //GET ALL
 app.get("/birds", (req, res)=>{
-
-    const birds = getBirds()
-
-    res.send( birds)
+    res.send( {data:  birds})
 })
 
 //GET ONE
-app.get("/birds:type", (req, res)=>{
-    //console.log(req.params)
-    res.send({message: "hello"})
-})//I can't make it work :(
+app.get("/birds/:id", (req, res)=>{
+    const bird = getBird(birds, Number(req.params.id))
+    res.send({data: bird})
+})
+
+//POST
+app.post('/birds', (req, res) => {
+    const bird = makeBird(getNextId(), req.body.type, req.body.family, req.body.breed)
+    res.send({data: bird})
+})
+
+//PACTH
+app.patch('/birds/:id', (req, res) => {
+    const bird = getBird(birds, Number(req.params.id))
+    bird = Object.assign(bird, req.body)
+    res.send( {data: bird})
+})
+
+//DELETE
+app.delete('/birds/:id', (req, res) => {
+    const bird = getBird(birds, Number(req.params.id))
+    const birdIndex = birds.indexOf(bird)
+    birds.splice(birdIndex, 1)
+    res.send({message: 'succcessfully deleted', bird: bird})
+})
 
 
-app.listen(8080)
+
+app.listen(8080, () => {
+    console.log('Server is running on port', 8080)//callback function
+})
